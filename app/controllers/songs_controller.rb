2f4285aa91217c772f96e10d9b47579df6765f4b
ozPop <ozziepopovas@gmail.com>
2016-09-04 16:44:43 -0400

    add delete functionality, jquery-ujs, update files
    update Gemfile adding jquery-ujs
    add application.js
    update application.html.erb
    update songs_controller with delete action
    add deleting songs with a link to show view

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,5 +1,5 @@
 class SongsController < ApplicationController
-  before_action :set_song!, only: [:show, :edit, :update]
+  before_action :set_song!, only: [:show, :edit, :update, :destroy]
 
   def index
     @songs = Song.all
@@ -30,6 +30,11 @@ class SongsController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def destroy
+    @song.destroy
+    redirect_to songs_url
   end
 
   private
